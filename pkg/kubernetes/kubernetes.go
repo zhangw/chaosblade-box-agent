7a81295ca67f9053e842a0a5719b75be37bc8576
张文 <jimokanghanchao@gmail.com>
2023-02-28 20:50:16 +0800

    test: trying to adjust flowcontrol

--- pkg/kubernetes/kubernetes.go
+++ pkg/kubernetes/kubernetes.go
@@ -59,7 +59,8 @@ func NewK8sClient() (*kubernetes.Clientset, error) {
 	if clusterConfig.Host != "" {
 		defaultClusterId = fmt.Sprintf("API_SERVER_%s", clusterConfig.Host)
 	}
-
+	clusterConfig.QPS = 50
+	clusterConfig.Burst = 100
 	options.Opts.SetClusterIdIfNotPresent(defaultClusterId)
 	clientset, err := kubernetes.NewForConfig(clusterConfig)
 	return clientset, err
